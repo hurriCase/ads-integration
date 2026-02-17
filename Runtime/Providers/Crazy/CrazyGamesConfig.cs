b5a75b2423e745df80640729dfc3131336b27499
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-02-17 13:57:16 +0300

    [fix] remove sealed from CrazyGamesConfig.cs

--- Runtime/Providers/Crazy/CrazyGamesConfig.cs
+++ Runtime/Providers/Crazy/CrazyGamesConfig.cs
@@ -6,7 +6,7 @@ using JetBrains.Annotations;
 namespace AdsIntegration.Runtime.Providers.Crazy
 {
     [PublicAPI]
-    public sealed class CrazyGamesConfig<TPlacement> : AdsConfigBase<TPlacement>
+    public class CrazyGamesConfig<TPlacement> : AdsConfigBase<TPlacement>
         where TPlacement : unmanaged, Enum { }
 }
 #endif
