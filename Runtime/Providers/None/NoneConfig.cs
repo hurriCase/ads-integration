64ccf63575bd770ccf4b193845908862c27ea713
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-02-17 06:18:44 +0300

    [fix] make NoneConfig public

--- Runtime/Providers/None/NoneConfig.cs
+++ Runtime/Providers/None/NoneConfig.cs
@@ -5,6 +5,6 @@ using CustomUtils.Runtime.AssetLoader;
 namespace AdsIntegration.Runtime.Providers.None
 {
     [Resource(FullSettingsPath, nameof(NoneConfig<TPlacement>), ResourceSettingsPath)]
-    internal sealed class NoneConfig<TPlacement> : AdsConfigBase<TPlacement>
+    public sealed class NoneConfig<TPlacement> : AdsConfigBase<TPlacement>
         where TPlacement : unmanaged, Enum { }
 }
