5e9d4bc4156716480f94c4017bcf63e4bb43b0ec
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-05-04 19:35:15 +0300

    refactor: LevelPlayConfig

--- Runtime/Providers/Unity/Data/LevelPlayConfig.cs
+++ Runtime/Providers/Unity/Data/LevelPlayConfig.cs
@@ -9,11 +9,11 @@ namespace AdsIntegration.Runtime.Providers.Unity.Data
     public class LevelPlayConfig<TPlacement> : AdsConfigBase<TPlacement>
         where TPlacement : unmanaged, Enum
     {
-        [field: SerializeField] private AdsData _androidAdsData;
-        [field: SerializeField] private AdsData _iosAdsData;
-
         [field: SerializeField] internal RetryConfig InterstitialConfig { get; private set; }
         [field: SerializeField] internal RetryConfig RewardedConfig { get; private set; }
+
+        [SerializeField] private AdsData _androidAdsData;
+        [SerializeField] private AdsData _iosAdsData;
 
         internal AdsData AdsData =>
 #if UNITY_ANDROID
